35a728e4091ab67e048a556707827e5592da6d85
ArthurAizawa <139519439+ArthurAizawa@users.noreply.github.com>
2025-04-22 23:48:25 -0300

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from './shared/header/header.component';
 import { BannerComponent } from './shared/banner/banner.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CardComponent } from './shared/card/card.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -14,13 +19,17 @@ import { CardComponent } from './shared/card/card.component';
     HeaderComponent,
     BannerComponent,
     FooterComponent,
-    CardComponent
+    CardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
